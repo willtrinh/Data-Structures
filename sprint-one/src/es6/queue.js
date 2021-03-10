fcc66b5fd89a9a78f4c6b0f7a8f67f72e72f31d5
Will Trinh <williamltrinh@gmail.com>
2021-03-10 12:31:55 -0800

    Implemented es6 stack & queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.queueSize = 0;
+    this.storage = {};
+    this.last = 0;
+    this.first = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.last++] = value;
+    this.queueSize++;
+  }
+
+  dequeue() {
+    if (this.queueSize > 0) {
+      var removedItem = this.storage[this.first];
+      delete this.storage[this.first++];
+      this.queueSize--;
+      return removedItem;
+    }
+  }
+
+  size() {
+    return this.queueSize;
+  }
 }
