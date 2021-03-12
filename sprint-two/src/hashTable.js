d6e05ee45544dcbfdb8dbbea9122b5c3d97f52bd
Will Trinh <williamltrinh@gmail.com>
2021-03-11 16:20:05 -0800

    Implemented hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,14 +5,38 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var arr = [k, v];
+  while (this._storage.get(index)) {
+    if (this._storage.get(index)[0] === k) {
+      break;
+    }
+    index++;
+  }
+  this._storage.set(index, arr);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  arr = this._storage.get(index);
+  if (arr === undefined) {
+    return undefined;
+  }
+  while (arr[0] !== k) {
+    index++;
+    arr = this._storage.get(index);
+  }
+  return arr[1];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each(function(value, i, storage) {
+    if (value) {
+      if (value[0] === k) {
+        delete storage[i];
+      }
+    }
+  });
 };
 
 
